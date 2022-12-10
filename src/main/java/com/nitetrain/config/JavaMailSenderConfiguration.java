180c00423cd0f21d34db7d7f35f2f4a8c2f9568c
Mirza Besic <besicm3@gmail.com>
2022-12-10 16:27:17 -0500

    deployed succesfully to aws, OMG this is not easy!

---
+++ src/main/java/com/nitetrain/config/JavaMailSenderConfiguration.java
@@ -0,0 +1,15 @@
+package com.nitetrain.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class JavaMailSenderConfiguration {
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        return new JavaMailSenderImpl();
+    }
+}
